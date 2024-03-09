6b01a47b86428fab3e8b2197e15dc21c3283788c
EduardPlesca <eplesca118@gmail.com>
2024-03-09 15:07:26 +0200

    Setup ListView builder legat de firebase.

--- lib/screens/landingPage.dart
+++ lib/screens/landingPage.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class LandingPage extends StatefulWidget {
@@ -10,6 +11,33 @@ class LandingPage extends StatefulWidget {
 class _LandingPageState extends State<LandingPage> {
   @override
   Widget build(BuildContext context) {
-    return const Text('Welcome landing screen');
+    return Scaffold(
+      body: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection("test_collection")
+              .snapshots(),
+          builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            }
+            if(!snapshot.hasData){
+              return const CircularProgressIndicator();
+            }
+            final userSnapshot = snapshot.data?.docs;
+            if (userSnapshot!.isEmpty) {
+              return const Text("no data");
+            }
+            return ListView.builder(
+                itemCount: userSnapshot.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    /*leading: CircleAvatar(
+                      child: userSnapshot[index]["test"],
+                    ),*/
+                    title: Text(userSnapshot[index]["test_field"]),
+                  );
+                });
+          }),
+    );
   }
 }
